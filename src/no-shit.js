502662ba9ece2a6633e8de1a307193e8a085be2e
Jakub Sadura <spam@sadura.pl>
2017-09-18 22:14:55 +0200

    Prevents usage of shit and fuck words in the code and comments

---
+++ src/no-shit.js
@@ -0,0 +1,48 @@
+const isOffensive = text => typeof text === 'string' && /(shit|fuck)/.test(text.toLowerCase())
+const reportWith = message => (context, node) => context.report(node, message)
+
+module.exports = {
+  meta: {
+    docs: {
+      description: 'Disallow use of offensive language',
+      category: 'Best Practices',
+      recommended: true,
+    },
+    schema: [
+      {
+        type: 'object',
+        properties: {
+          nsfw: {
+            type: 'boolean',
+          },
+        },
+      },
+    ],
+  },
+  create(context) {
+    const config = context.options[0] || {}
+    const {nsfw} = config
+    const message = nsfw ?
+      'It would be great if you could be more polite my darling...' :
+      'Watch your language, motherfucker!'
+    const report = reportWith(message)
+    return {
+      Literal(node) {
+        isOffensive(node.value) && report(context, node)
+      },
+      Identifier(node) {
+        isOffensive(node.name) && report(context, node)
+      },
+      TemplateElement(node) {
+        isOffensive(node.value.raw) && report(context, node)
+      },
+      Program(node) {
+        node.comments
+          .filter(c => c.type !== 'Shebang')
+          .forEach(c => {
+            isOffensive(c.value) && report(context, c)
+          })
+      },
+    }
+  },
+}
---
+++ src/no-shit.test.js
@@ -0,0 +1,45 @@
+const {RuleTester} = require('eslint')
+const rule = require('./no-shit')
+
+const ruleTester = new RuleTester()
+ruleTester.run('no-shit', rule, {
+  valid: [
+    '"no sht"',
+    '"NO FCK"',
+    'console.log(123)',
+    {code: 'var shhhh = 1', options: [{nsfw: true}]},
+  ],
+  invalid: [
+    invalid('"FUCK"'),
+    invalid('"SHIT"'),
+    invalid('"no shit"'),
+    invalid('function noFuck() {}'),
+    {
+      code: `
+        var shit = 'foo'
+        var bat = 'fuck'
+      `,
+      errors: 2,
+    },
+    {
+      code: 'var shit = 1',
+      options: [
+        {nsfw: true},
+      ],
+      errors: [
+        {message: 'It would be great if you could be more polite my darling...'},
+      ],
+    },
+  ],
+})
+
+function invalid(code, output) {
+  const invalidTest = {
+    code,
+    errors: [{message: 'Watch your language, motherfucker!'}],
+  }
+  if (output) {
+    invalidTest.output = output
+  }
+  return invalidTest
+}
